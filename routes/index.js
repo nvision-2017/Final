99827d3e217dae18eccd764007669aacdd1af69e
Prateek Kumar <prateek@prateekkumar.in>
2016-12-20 21:14:15 +0530

    merge

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
-var keystone = require('keystone'),
+const keystone = require('keystone'),
     middleware = require('./middleware'),
-    importRoutes = keystone.importer(__dirname);
+    importRoutes = keystone.importer(__dirname),
+    User = keystone.list('User');
+
+const jwt = require('jsonwebtoken');
+
+let tokenSecret = 'varyverysecrettokenithinkso';
 
 const Event = keystone.list('Event');
 
@@ -10,17 +15,19 @@ keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
 
 const routes = {
-  views: importRoutes('./views'),
-  domains: require('./api/domains')
+    views: importRoutes('./views'),
+    domains: require('./api/domains')
 }
 
+const api = require('./api/users');
+
 // Handle 404 errors
-keystone.set('404', function(req, res, next) {
+keystone.set('404', function (req, res, next) {
     res.notfound();
 });
 
 // Handle other errors
-keystone.set('500', function(err, req, res, next) {
+keystone.set('500', function (err, req, res, next) {
     var title, message;
     if (err instanceof Error) {
         message = err.message;
@@ -30,7 +37,7 @@ keystone.set('500', function(err, req, res, next) {
 });
 
 // Bind Routes
-exports = module.exports = function(app) {
+exports = module.exports = function (app) {
     // views
     app.get('/mobile', (req, res) => {
         res.send(require('../lib/detectmobilebrowser')(req));
@@ -42,19 +49,61 @@ exports = module.exports = function(app) {
     app.get('/team', routes.views.team);
     app.get('/workshops', routes.views.workshops);
     app.get('/exhibitions', routes.views.exhibitions);
+    // app.get('/register', routes.views.register);
     app.get('/events/:event', (req, res) => {
-        Event.model.findOne({link: `/events/${req.params.event}`})
-        .then(e => {
-            if (!e) return res.notfound();
-            var view = new keystone.View(req, res);
-            view.render('event', e);
-        }, e => res.err(e));
+        Event.model.findOne({ link: `/events/${req.params.event}` })
+            .then(e => {
+                if (!e) return res.notfound();
+                var view = new keystone.View(req, res);
+                view.render('event', e);
+            }, e => res.err(e));
     });
 
-  // Domain API
-  app.get('/domains', routes.domains.getDomains);
-  app.get('/domains/:domain', routes.domains.getDomain);
-  app.get('/domains/:domain/events', routes.domains.getEventsOfDomain);
-  app.get('/domains/:domain/events/:event', routes.domains.getEvent);
-  
+    // app.post('/signup', (req, res) => {
+    //     return res.send('coming soon ...');
+    //     new User.model({
+    //         name: { first: req.body.first, last: req.body.last },
+    //         email: req.body.email,
+    //         password: req.body.password,
+    //         canAccessKeystone: false
+    //     }).save(() => { res.send('done') });
+    // });
+
+    app.post('/api/auth', (req, res)=>{
+        keystone.session.signin({
+            email: req.body.email,
+            password: req.body.password
+        }, req, res, (user)=>{
+            console.log(user);
+            if (!user) return res.json({success: false, message: "Invalid credentials"});
+            var token = jwt.sign(user, tokenSecret, {expiresIn: 3600});
+            return res.json({success:true, message: 'Auth success', token: token});
+        }, (err) => res.json({success: false, message: "Auth failed"}));
+    });
+
+    app.use('/api/*', function(req, res, next){
+        let token = req.body.token || req.query.token || req.headers['x-access-token'];
+        if (token) {
+            jwt.verify(token, tokenSecret, function(err, decoded){
+                if (err) {
+                    return res.json({success: false, message: 'Auth failed'});
+                }
+                else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            return res.status(403).json({success: false, message: 'Token required'});
+        }
+    });
+
+    app.get('/api/user', api.getUser);
+
+    // Domain API
+    app.get('/domains', routes.domains.getDomains);
+    app.get('/domains/:domain', routes.domains.getDomain);
+    app.get('/domains/:domain/events', routes.domains.getEventsOfDomain);
+    app.get('/domains/:domain/events/:event', routes.domains.getEvent);
+
 };
