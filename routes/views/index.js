f9b0fe884fd027cb81bbe2035c7e8d7103f811f0
thecodesome <cs15btech11018@iith.ac.in>
2016-12-25 15:30:43 +0530

    index in mobile

--- routes/views/index.js
+++ routes/views/index.js
@@ -4,7 +4,7 @@ exports = module.exports = function(req, res) {
 
     var view = new keystone.View(req, res);
     if (require('../../lib/detectmobilebrowser')(req))
-        view.render('index_mobile', {user: req.user});
+        view.render('index_mobile', {user: req.user, updates: keystone.get('updatesWeb')});
     else
         view.render('index', {user: req.user, updates: keystone.get('updatesWeb')});
 
