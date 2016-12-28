eb2a39f4ac37abe182368f98e53318f7f8867ac3
thecodesome <cs15btech11018@iith.ac.in>
2016-12-28 16:22:17 +0530

    updates

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 module.exports = exports = [
     '<strong>1:</strong> Registrations for events have started. <a href="/signin">Signup</a> now!',
-    '<strong>2:</strong> Check out the upcoming <a href="/workshops">workshops</a> on <a href="/iotDuino">IoT Duino</a> and <a href="/mobileMaking">Mobile Making</a>'
+    '<strong>2:</strong> Check out the upcoming <a href="/workshops">workshops</a> on <a href="/iotDuino">IoT Duino</a>, <a href="/mobileMaking">Mobile Making</a> and <a href="/processingMech">Project Processing Machinery</a>'
 ];
