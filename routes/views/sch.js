0b27373a998056fd5791342e96963e4af400f900
thecodesome <cs15btech11018@iith.ac.in>
2017-01-15 11:57:25 +0530

    schedule sheet

--- routes/views/sch.js
+++ routes/views/sch.js
@@ -2,8 +2,8 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 
-    var view = new keystone.View(req, res);
-
-    view.render('sch', {user: req.user, updates: keystone.get('updatesWeb')});
+    res.redirect('https://docs.google.com/spreadsheets/d/12pCAHPzVPc9lwnXQew9vIS7O9wSRhryXHRBROgf0lkg/pubhtml?gid=764768582&single=true&widget=true&headers=false');
+    // var view = new keystone.View(req, res);
+    // view.render('sch', {user: req.user, updates: keystone.get('updatesWeb')});
 
 }
