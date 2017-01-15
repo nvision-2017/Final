e4fe6ae54c5e81ac312e5727e185e85d659131ae
thecodesome <cs15btech11018@iith.ac.in>
2017-01-15 20:44:15 +0530

    transport

--- routes/index.js
+++ routes/index.js
@@ -128,6 +128,7 @@ exports = module.exports = function (app) {
     app.get('/emblazon', routes.views.emblazon);
     app.get('/ProShows', routes.views.ProShows);
     app.get('/schedule', routes.views.sch);
+    app.get('/transport', routes.views.trans);
     app.get('/dashboard', signinSSO, (req, res)=>{
         if (!req.user) {
             return res.redirect(process.env.ID_SERVER+'/signin?url='+process.env.THIS_SERVER+'/dashboard');
