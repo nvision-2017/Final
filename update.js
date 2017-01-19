957dd12f812ce69520711de47e76b06ddb41632a
thecodesome <cs15btech11018@iith.ac.in>
2017-01-19 15:44:53 +0530

    updates

--- update.js
+++ update.js
@@ -1,4 +1,5 @@
 module.exports = exports = [
     '<strong>1:</strong> <a href="https://elmatrico.nvision.org.in/">El-Matrico</a>, the most interesting and intense online MatLab coding event is here! Visit <a href="https://elmatrico.nvision.org.in/">https://elmatrico.nvision.org.in/</a> for more details.',
     '<strong>2:</strong> Bring your own laptops for Hack A Maze',
+    '<strong>3:</strong> Check out <a href="http://www.meraevents.com/event/elan-and-nvision-2017-camping-night" target="_blank">camping and bonfire</a> at &eta;vision and Elan',
 ];
